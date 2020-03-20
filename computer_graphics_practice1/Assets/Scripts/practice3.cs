99646dddb3f12d5d0333f004c049ca9bbf9a677a
ilyatim <31735653+ilyatim@users.noreply.github.com>
2020-03-20 19:05:18 +0300

    practice 6 finish

---
+++ computer_graphics_practice1/Assets/Scripts/practice3.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class practice3 : MonoBehaviour
+{
+    public float RandomNumber;
+
+    public GameObject RandomObject;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log(RandomNumber);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Randomize();
+    }
+
+    private void Randomize()
+    {
+        RandomNumber = Random.Range(1, 10);
+        Debug.Log(RandomNumber);
+        if (RandomNumber == 3) Debug.Log("Проверка условия");
+    }
+}
