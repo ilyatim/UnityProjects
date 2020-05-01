371f1b6c5352f50cf35702a70a977fec27ad718d
ilyatim <31735653+ilyatim@users.noreply.github.com>
2020-05-01 22:03:11 +0300

    practice 11-12 add

---
+++ computer_graphics_practice1/Assets/Scripts/practice11/ButtonScript.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class ButtonScript : MonoBehaviour
+{
+    public GameObject gameObjectPanel;
+    public GameObject gameObjectImage;
+    public Sprite NewSprite;
+
+    public void Close()
+    {
+        gameObjectPanel.SetActive(false);
+    }
+
+    public void ChangeImage()
+    {
+        gameObjectImage.GetComponent<Image>().sprite = NewSprite;
+    }
+}
---
+++ computer_graphics_practice1/Assets/Scripts/practice12/MenuScript.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+public class MenuScript : MonoBehaviour
+{
+    public GameObject menuWindow;
+    public void StartScene()
+    {
+        Application.LoadLevel("scene2");
+    }
+
+    public void Options(GameObject window)
+    {
+        window.SetActive(true);
+        menuWindow.SetActive(false);
+    }
+
+    public void Quit()
+    {
+        Application.Quit();
+    }
+
+    public void SetMenuActive()
+    {
+        menuWindow.SetActive(true);
+    }
+}
