4eab5a040d580a670db96dfae52c8609a7a3b7ce
ilyatim <31735653+ilyatim@users.noreply.github.com>
2020-05-03 19:25:57 +0300

    lab 3.2 update

---
+++ computer_graphics_lab3/Assets/Scripts/Navigation.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Navigation : MonoBehaviour
+{
+    public void OpenC(GameObject gameObjectC)
+    {
+        gameObjectC.SetActive(true);
+    }
+    public void OpenD(GameObject gameObjectD)
+    {
+        gameObjectD.SetActive(true);
+    }
+}
